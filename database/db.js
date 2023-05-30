c2b9e192d4f68d207556faf3b09c1d502e8429bc
Oscar Suarez <Oscar.suarezwgd@gmail.com>
2023-05-29 23:52:59 -0600

    Se agregó la variable de entorno para conectarse a CloudAtlas de mongoDB

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const MONGO_URL = 'mongodb://127.0.0.1:27017/bd';
-const RENDER_URL = 'mongodb://https://poketimer-back.onrender.com';
+const RENDER_URL = process.env.RENDER_URL;
 
 
 const db = async () =>{
