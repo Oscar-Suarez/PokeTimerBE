db7387cec7b83ec166ce4cd00740907d002c2df5
Oscar Suarez <Oscar.suarezwgd@gmail.com>
2023-05-29 19:59:14 -0600

    Agregando el link de Render

--- database/db.js
+++ database/db.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
-const MONGO_URL = 'mongodb://127.0.0.1:27017/bd ';
+const MONGO_URL = 'mongodb://127.0.0.1:27017/bd';
+const RENDER_URL = 'https://poketimer-back.onrender.com';
+
+const db = async () => {
+    let connectionString = MONGO_URL;
 
 
-const db = async () =>{
-    await mongoose.connect(MONGO_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        keepAlive: true,
-    })
-    .then(() => console.log('Contectado a db'))
-    .catch((error) => console.log(error));
-}
+    if (process.env.RENDER) {
+        connectionString = RENDER_URL;
+    }
+
+    await mongoose
+        .connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            keepAlive: true,
+        })
+        .then(() => console.log('Conectado a la base de datos'))
+        .catch((error) => console.log(error));
+};
 
 module.exports = db;
