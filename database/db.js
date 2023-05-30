8d9cde9e1563cf4d72be01ee99852923b7192472
Oscar Suarez <Oscar.suarezwgd@gmail.com>
2023-05-29 23:30:11 -0600

    Posible solución a problema de conexión

--- database/db.js
+++ database/db.js
@@ -1,23 +1,16 @@
 const mongoose = require('mongoose');
-const MONGO_URL = 'mongodb://127.0.0.1:27017/bd';
+// const MONGO_URL = 'mongodb://127.0.0.1:27017/bd';
 const RENDER_URL = 'mongodb://https://poketimer-back.onrender.com';
 
-const db = async () => {
-    let connectionString = MONGO_URL;
 
-
-    if (process.env.RENDER) {
-        connectionString = RENDER_URL;
-    }
-
-    await mongoose
-        .connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            keepAlive: true,
-        })
-        .then(() => console.log('Conectado a la base de datos'))
-        .catch((error) => console.log(error));
-};
+const db = async () =>{
+    await mongoose.connect(RENDER_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        keepAlive: true,
+    })
+    .then(() => console.log('Contectado a db'))
+    .catch((error) => console.log(error));
+}
 
 module.exports = db;
